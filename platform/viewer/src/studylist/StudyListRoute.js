1f8ce3ff500e3501e37885c4c1dbddbdb74902ae
jinyan <jinyan@neuro42.com>
2023-02-26 17:02:30 -0800

    change table name

--- platform/viewer/src/studylist/StudyListRoute.js
+++ platform/viewer/src/studylist/StudyListRoute.js
@@ -229,7 +229,7 @@ function StudyListRoute(props) {
       <div className="study-list-header">
         <div className="header">
           <h1 style={{ fontWeight: 300, fontSize: '22px' }}>
-            {t('StudyList')}
+            {t('PatientList')}
           </h1>
         </div>
         <div className="actions">
